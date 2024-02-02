38a67a1d4077ffffcc37a9910fa653a6e3d5936a
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-02-03 00:03:23 +0800

    update to latest version

--- common/src/main/java/org/thinkingstudio/mio_zoomer/MioZoomerPreLaunchMod.java
+++ common/src/main/java/org/thinkingstudio/mio_zoomer/MioZoomerPreLaunchMod.java
@@ -1,11 +1,11 @@
 package org.thinkingstudio.mio_zoomer;
 
-import ho.artisan.azusa_config.shadow.quiltconfig.api.annotations.ConfigFieldAnnotationProcessors;
+import ho.artisan.azusa_config.shadow.quiltconfig.api.annotations.ConfigFieldAnnotationProcessor;
 
 import org.thinkingstudio.mio_zoomer.config.metadata.WidgetSize;
 
 public class MioZoomerPreLaunchMod {
 	public static void onPreLaunch() {
-		ConfigFieldAnnotationProcessors.register(WidgetSize.class, new WidgetSize.Processor());
+		ConfigFieldAnnotationProcessor.register(WidgetSize.class, new WidgetSize.Processor());
 	}
 }
